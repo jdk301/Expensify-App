603ff34784a70b921eedfc0beb8ffc7e1722fb86
Justin Kim <justinkim0710@gmail.com>
2021-01-20 18:27:38 -0500

    Setup Login page and google authentication, making changes to app.js, AppRouter.js, firebase.js, & creating auth.js, LoginPage.js, and LoginPage.test.js files

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,18 +1,20 @@
 import React from "react";
 import { BrowserRouter, Route, Switch, Link, NavLink } from "react-router-dom";
-import ExpenseDashboardPage from "../components/ExpenseDashboardPage.js";
-import AddExpensePage from "../components/AddExpensePage.js";
-import EditExpensePage from "../components/EditExpensePage.js";
-import HelpPage from "../components/HelpPage.js";
-import NotFoundPage from "../components/NotFoundPage.js";
-import Header from "../components/Header.js";
+import LoginPage from "../components/LoginPage";
+import ExpenseDashboardPage from "../components/ExpenseDashboardPage";
+import AddExpensePage from "../components/AddExpensePage";
+import EditExpensePage from "../components/EditExpensePage";
+import HelpPage from "../components/HelpPage";
+import NotFoundPage from "../components/NotFoundPage";
+import Header from "../components/Header";
 
 const AppRouter = () => (
   <BrowserRouter>
     <div>
       <Header />
       <Switch>
-        <Route path="/" component={ExpenseDashboardPage} exact={true} />
+        <Route path="/" component={LoginPage} exact={true} />
+        <Route path="/dashboard" component={ExpenseDashboardPage} />
         <Route path="/create" component={AddExpensePage} />
         <Route path="/edit/:id" component={EditExpensePage} />
         <Route path="/help" component={HelpPage} />
